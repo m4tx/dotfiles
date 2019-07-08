76844cfb3b5e67c2f06c27c3680854e82e633aca
Mateusz Maćkowski <m4tx@m4tx.pl>
2019-07-09 00:45:42 +0200

    Update i3 config

--- .i3/lock.sh
+++ .i3/lock.sh
@@ -1,17 +1,4 @@
 #!/bin/sh -e
 
+i3lock -c 000000
 xset dpms force off
-
-# Take a screenshot
-scrot /tmp/screen_locked.png
-
-# Pixellate it 10x
-#mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
-convert /tmp/screen_locked.png -blur 0x12 /tmp/screen_locked.png
-
-# Lock screen displaying this image.
-i3lock -i /tmp/screen_locked.png
-xset dpms force off
-
-# Turn the screen off after a delay.
-sleep 15; pgrep i3lock && xset dpms force off
